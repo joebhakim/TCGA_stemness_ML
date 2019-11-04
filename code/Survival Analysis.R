598f6d2b312102b44a880099fbf24ca0655ff22f
joebhakim <jhakim3@jhu.edu>
2019-11-04 12:06:15 -0500

    integrating changes into one file

--- code/Survival Analysis.R
+++ code/Survival Analysis.R
@@ -30,24 +30,31 @@ dat[,c("mRNAsi", "mDNAsi", "EREG_mRNAsi")] <- 100*dat[,c("mRNAsi", "mDNAsi", "ER
 
 ##Kaplan-Meier Curves (examples with gender, dichotomized age, dichotomized EREG_mRNAsi given)
 surv.object <- Surv(time = dat$Y, event = 1 - dat$censor.indicator) #event = 1 if died, 0 if censored (alive at end of follow-up)
-dat <- dat %>% mutate(age_group = ifelse(age >=50, "Old", "Young")) #Dichotomize age
-dat <- dat %>% mutate(risk_group = ifelse(EREG_mRNAsi >= median(EREG_mRNAsi), "High Risk", "Low Risk")) #Dichotomize EREG_mRNAsi
+dat <- dat %>% mutate(age_group = ifelse(age >=50, "50 and Older", "Younger than 50")) #Dichotomize age
+dat <- dat %>% mutate(risk_group = ifelse(EREG_mRNAsi >= median(EREG_mRNAsi), "Above-Median Stemness", "Below-Median Stemness")) #Dichotomize EREG_mRNAsi
+dat <- dat %>% mutate(mDNAsi_group = ifelse(mDNAsi >= median(mDNAsi), "Above-Median Stemness from Methylation", "Below-Median Stemness from Methylation")) #Dichotomize mDNAsi
+dat <- dat %>% mutate(mRNSsi_group = ifelse(mRNAsi >= median(mRNAsi), "Above-Median Stemness from Gene Expression", "Below-Median Stemness from Gene Expression")) #Dichotomize mDNAsi
+
 
 km.curve <- survfit(surv.object ~ gender, data = dat)
-ggsurvplot(km.curve, data = dat, pval = TRUE) + xlab("Time (days post diagnosis)") #pval should correspond to log-rank test
+ggsurvplot(km.curve, data = dat, pval = TRUE, legend.labs=c("Female", "Male")) + xlab("Time (days post diagnosis)") #pval should correspond to log-rank test
 surv_pvalue(km.curve, data = dat, method = "survdiff")
 
 km.curve <- survfit(surv.object ~ age_group, data = dat)
-ggsurvplot(km.curve, data = dat, pval = TRUE) + xlab("Time (days post diagnosis)")
+ggsurvplot(km.curve, data = dat, pval = TRUE, legend.labs=c("50 and Older", "Younger than 50")) + xlab("Time (days post diagnosis)")
 surv_pvalue(km.curve, data = dat, method = "survdiff")
 
 km.curve <- survfit(surv.object ~ risk_group, data = dat)
-ggsurvplot(km.curve, data = dat, pval = TRUE) + xlab("Time (days post diagnosis)")
+ggsurvplot(km.curve, data = dat, pval = TRUE, legend.labs=c("Above-Median Stemness", "Below-Median Stemness")) + xlab("Time (days post diagnosis)")
+surv_pvalue(km.curve, data = dat, method = "survdiff")
+
+km.curve <- survfit(surv.object ~ mDNAsi_group, data = dat)
+ggsurvplot(km.curve, data = dat, pval = TRUE, legend.labs = c("Above-Median Stemness from Methylation", "Below-Median Stemness from Methylation")) + xlab("Time (days post diagnosis)")
 surv_pvalue(km.curve, data = dat, method = "survdiff")
 
 ##Cox Proportional-Hazards Model
 ##EREG-mRNAsi could be used instead of the two separate RNA and DNA stemness indices
-fit.coxph <- coxph(surv.object ~  mRNAsi + mDNAsi + gender + age + cancer.type, data = dat)
+fit.coxph <- coxph(surv.object ~  mRNAsi + mDNAsi + gender + age + cancer.class, data = dat)
 summary(fit.coxph)
 ggforest(fit.coxph, data = dat)
 
--- code/wrangling-data.R
+++ code/wrangling-data.R
@@ -16,5 +16,20 @@ dat <- pd.450.prim %>%
   filter(!(is.na(days_to_death) & is.na(days_to_last_followup))) %>%
   mutate(Y      = ifelse(!is.na(days_to_death), days_to_death, days_to_last_followup),
          censor.indicator = ifelse(!is.na(days_to_death), 1, 0)) %>%
+  mutate(cancer.class = case_when(
+      cancer.type %in% c('LAML', 'DLBC', 'THYM') ~ 'hematologic_lymphatic',
+      cancer.type %in% c('OV', 'UCEC', 'CESC', 'BRCA') ~ 'solid, gynecologic',
+      cancer.type %in% c('BLCA', 'PRAD', 'TGCT', 'KIRC', 'KICH', 'KIRP') ~ 'solic, urologic',
+      cancer.type %in% c('THCA', 'ACC') ~ 'solid, endocrine',
+      cancer.type %in% c('ESCA', 'STAD', 'COAD', 'READ') ~ 'solid, core GI',
+      cancer.type %in% c('LIHC', 'PAAD', 'CHOL') ~ 'developmental GI',
+      cancer.type %in% c('HNSC') ~ 'head and neck',
+      cancer.type %in% c('LUAD', 'LUSC', 'MESO') ~ 'thoracic',
+      cancer.type %in% c('GBM', 'LGG') ~ 'Central nervous system',
+      cancer.type %in% c('SARC', 'UCS') ~ 'soft tissue',
+      cancer.type %in% c('PCPG') ~ 'neural crest',
+      cancer.type %in% c('SKCM', 'UVM') ~ 'melanocytic'
+    )
+  ) %>%
   select(-days_to_death, -days_to_last_followup)
 save(dat, file="data/tcga_wrangled.rda", compress="xz")
