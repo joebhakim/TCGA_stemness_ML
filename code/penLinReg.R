7425161133d81bc6352e3329a71fa749f434900c
Matthew Quinn <mjq522@g.harvard.edu>
2019-11-25 17:19:59 -0500

    rough draft

--- code/penLinReg.R
+++ code/penLinReg.R
@@ -1,10 +1,16 @@
 #Penalized linear regression
 library(gelnet)
+#library(glmnet)
 
 #Inputs: design matrix, boolean for using mDNAsi, boolean for mRNAsi, boolean for raw genetic data, l1 penalty, l2 penalty. 
 #Output: fitted model
 
+#dat <- dat[which(dat$censor.indicator == 0),]
+
 getPenLinReg <- function(dat, mDNAsi=F, mRNAsi=F, raw=T, l1=1, l2=1, max.iter = 100, eps = 1e-05){
+  #Remove rows with missing values
+  dat <- na.omit(dat)
+  
   #Extract outcome
   daysToDeath <- dat[,"Y"]
   
@@ -26,13 +32,16 @@ getPenLinReg <- function(dat, mDNAsi=F, mRNAsi=F, raw=T, l1=1, l2=1, max.iter = 
     design <- cbind(design, dat[,rawColNames])
   }
   
-  
   #sapply(design, class)
   
-  design <- as.matrix(design)
+  design <- model.matrix( ~ ., design)
+  
+  #design <- as.matrix(design)
   
   #https://cran.r-project.org/web/packages/gelnet/gelnet.pdf
   elasticFit <- gelnet(X=design, y=daysToDeath, l1=l1, l2=l2, max.iter=max.iter, eps=eps)
+  
+  #elasticFit <- glmnet(x=design, y=daysToDeath, alpha=0.5)
   
   return(elasticFit)
 }
