fc4c98e429934d87699d2eefba8b443f57af8130
Matthew Quinn <mjq522@g.harvard.edu>
2019-11-25 17:45:46 -0500

    updated penalized lin reg

--- code/penLinReg.R
+++ code/penLinReg.R
@@ -5,43 +5,31 @@ library(gelnet)
 #Inputs: design matrix, boolean for using mDNAsi, boolean for mRNAsi, boolean for raw genetic data, l1 penalty, l2 penalty. 
 #Output: fitted model
 
-#dat <- dat[which(dat$censor.indicator == 0),]
-
-getPenLinReg <- function(dat, mDNAsi=F, mRNAsi=F, raw=T, l1=1, l2=1, max.iter = 100, eps = 1e-05){
+getPenLinReg <- function(design, mDNAsi=F, mRNAsi=F, raw=T, l1=1, l2=1, max.iter = 100, eps = 1e-05){
   #Remove rows with missing values
-  dat <- na.omit(dat)
+  #design <- na.omit(design)
   
   #Extract outcome
-  daysToDeath <- dat[,"Y"]
+  daysToDeath <- design[,"Y"]
   
   #Extract design matrix
-  design <- dat[,c("gender", "age", "cancer.class")]
+  cancerCols <- colnames(design)[grepl("cancer", colnames(design))]
+  designReduced <- design[,c("genderMALE", "age", cancerCols)]
   
   if(mDNAsi){
-    design <- cbind(design, dat[,"mDNAsi"])
-    colnames(design)[length(colnames(design))] <- "mDNAsi"
-  }
-  
-  if(mDNAsi){
-    design <- cbind(design, dat[,"mRNAsi"])
-    colnames(design)[length(colnames(design))] <- "mRNAsi"
+    designReduced  <- cbind(designReduced , design[,"mDNAsi"])
+    colnames(designReduced)[length(colnames(designReduced))] <- "mDNAsi"
   }
   
   if(raw){
-    rawColNames <- colnames(dat)[grepl("cg", names(dat))]
-    design <- cbind(design, dat[,rawColNames])
+    rawColNames <- colnames(design)[grepl("cg", colnames(design))]
+    designReduced  <- cbind(designReduced, design[,rawColNames])
   }
   
-  #sapply(design, class)
-  
-  design <- model.matrix( ~ ., design)
-  
-  #design <- as.matrix(design)
-  
   #https://cran.r-project.org/web/packages/gelnet/gelnet.pdf
-  elasticFit <- gelnet(X=design, y=daysToDeath, l1=l1, l2=l2, max.iter=max.iter, eps=eps)
+  elasticFit <- gelnet(X=designReduced, y=daysToDeath, l1=l1, l2=l2, max.iter=max.iter, eps=eps)
   
-  #elasticFit <- glmnet(x=design, y=daysToDeath, alpha=0.5)
+  #elasticFit <- glmnet(x=designReduced, y=daysToDeath, alpha=0.5)
   
   return(elasticFit)
 }
